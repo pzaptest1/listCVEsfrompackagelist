dd9a60eaf35bd1f1ff364ee64ebd715d3f27f533
pzaptest1 <pali_surdhar@yahoo.co.uk>
2023-05-17 14:58:55 +0100

    Update getcve.py
    added capability to parse a spreadsheet

--- getcve.py
+++ getcve.py
@@ -1,16 +1,22 @@
 import requests
+import pandas as pd
 
 
-def parse_oss_packages(packages):
+def parse_oss_packages_from_spreadsheet(spreadsheet_path):
+    df = pd.read_excel(spreadsheet_path)
+    packages = df.values.tolist()
+
     for package in packages:
-        print(f"Searching for CVEs in package: {package}")
+        package_name = str(package[0])
+        package_version = str(package[1])
+        print(f"Searching for CVEs in package: {package_name} {package_version}")
 
-        cve_results = search_cves(package)
+        cve_results = search_cves(package_name)
         if cve_results:
             for cve in cve_results:
                 cve_id = cve['cve']['CVE_data_meta']['ID']
                 cvss = cve.get('impact', {}).get('baseMetricV3', {}).get('cvssV3', {}).get('baseScore')
-                print(f"Package: {package} | CVE: {cve_id} | CVSS: {cvss}")
+                print(f"Package: {package_name} {package_version} | CVE: {cve_id} | CVSS: {cvss}")
 
         print("---")
 
@@ -27,11 +33,5 @@ def search_cves(package):
 
 
 # Example usage
-oss_packages = [
-    "openssl v1.0.2",
-    "openssl v1.1.1",
-    "python 3.10",
-    "Boost Libraries 1.74.0"
-]
-parse_oss_packages(oss_packages)
-
+spreadsheet_path = "path/to/your/spreadsheet.xlsx"
+parse_oss_packages_from_spreadsheet(spreadsheet_path)
